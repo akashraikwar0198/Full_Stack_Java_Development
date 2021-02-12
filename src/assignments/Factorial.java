b166034d9907f3ae8c7048fdc6e79d34a3729aad
akashraikwar0198 <akashrrr1@gmail.com>
2021-02-12 15:09:10 +0530

    factorial program corrected.

--- src/assignments/Factorial.java
+++ src/assignments/Factorial.java
@@ -4,15 +4,16 @@ import java.util.Scanner;
 
 public class Factorial {
     public static void main(String[] args) {
-        int num, factorial=1;
+
+        int factorial = 1;
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number to find factorial : ");
 
-        num = scanner.nextInt();
+        int num = scanner.nextInt();
 
-        for (int i=1; i<=num; i++) {
-            factorial = factorial * num;
+        for (int i = 1; i<=num; i++) {
+            factorial = factorial * i;
         }
         System.out.println("Factorial of " + num + " is : " + factorial);
     }
