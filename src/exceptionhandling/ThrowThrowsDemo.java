4ff35d91aaec616150b952e8eecedb1b24dc8e2a
akashraikwar0198 <akashrrr1@gmail.com>
2021-02-16 13:39:47 +0530

    throw and throws keyword program

---
+++ src/exceptionhandling/ThrowThrowsDemo.java
@@ -0,0 +1,15 @@
+package exceptionhandling;
+
+public class ThrowThrowsDemo {
+
+    public static void main(String[] args) throws Exception{
+
+        int i = 17;
+
+        if (i<18){
+            throw new Exception("You cannot vote");
+        }
+        else
+            System.out.println("You can vote.");
+    }
+}
