1c974e3c56246eb4950b3c1666d5167e89ee38a7
akashraikwar0198 <akashrrr1@gmail.com>
2021-02-19 13:33:19 +0530

    File Reader & Writer program, Try with Resource added.

---
+++ src/filehandling/FileWriterAndReaderDemo.java
@@ -0,0 +1,41 @@
+package filehandling;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class FileWriterAndReaderDemo {
+
+    public static void main(String[] args) throws Exception  {
+        FileWriter fileWriter = null;
+        FileReader fileReader = null;
+
+        //to write a file
+        try{
+            fileWriter = new FileWriter("FileWriter.txt");
+            fileWriter.write("Today is Friday. Chhatrapati Shivaji Maharaj Jayanti. Jai Shivaji.");
+            System.out.println("File is written successfully");
+            fileWriter.close();
+        } catch (IOException ioException){
+            ioException.printStackTrace();
+        } finally {
+            fileWriter.close();
+        }
+
+        //To read a file
+        try{
+            fileReader = new FileReader("FileWriter.txt");
+            int i = fileReader.read();
+
+            while(i>0){
+                System.out.print((char)i);
+                i = fileReader.read();
+            }
+        } catch (IOException fileNotFoundException){
+            fileNotFoundException.printStackTrace();
+        } finally {
+            fileReader.close();
+        }
+
+    }
+}
---
+++ src/filehandling/TryWithResource.java
@@ -0,0 +1,33 @@
+package filehandling;
+
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class TryWithResource {
+
+
+    public static void main(String[] args) throws Exception {
+
+        //to write a file
+        try (FileWriter fileWriter = new FileWriter("FileWriter.txt")) {
+
+            fileWriter.write("Today is Friday. Chhatrapati Shivaji Maharaj Jayanti. Jai Shivaji.");
+            System.out.println("File is written successfully");
+        } catch (IOException ioException) {
+            ioException.printStackTrace();
+        }
+
+        //To read a file
+        try (FileReader fileReader = new FileReader("FileWriter.txt")) {
+            int i = fileReader.read();
+            while (i > 0) {
+                System.out.print((char) i);
+                i = fileReader.read();
+            }
+        } catch (IOException fileNotFoundException) {
+            fileNotFoundException.printStackTrace();
+        }
+    }
+}
+
