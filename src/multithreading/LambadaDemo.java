8e443cc624ba0135674b4b8428af86f996587632
akashraikwar0198 <akashrrr1@gmail.com>
2021-02-24 13:38:15 +0530

    methods of threads added in the RunnableDemo Program.

--- src/multithreading/LambadaDemo.java
+++ src/multithreading/LambadaDemo.java
@@ -4,13 +4,12 @@ public class LambadaDemo {
 
     public static void main(String[] args)
     {
-        LambadaInterface s =(str1,str2)-> str1+str2;
-        System.out.println(s.concat("Pune","Mumbai"));
-
+        LambadaInterface s = (str1,str2) -> str1+str2;
+        System.out.println(s.concat("Pune"," the Best City"));
     }
 }
 
 @FunctionalInterface
-interface LambadaInterface{
-    public String concat(String a, String b);
+interface LambadaInterface{             //only one abstract method (functional interface)
+    String concat(String a, String b);
 }
--- src/multithreading/RunnableDemo.java
+++ src/multithreading/RunnableDemo.java
@@ -2,9 +2,9 @@ package multithreading;
 
 public class RunnableDemo {
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) throws InterruptedException {
         Thread t1 = new Thread(() -> {
+            System.out.println(Thread.currentThread().isAlive());
             for (int i = 1; i <= 5; i++)
             {
                 System.out.println("Hii");
@@ -14,9 +14,10 @@ public class RunnableDemo {
                     e.printStackTrace();
                 }
             }
-        });
+        },"First_Thread");
 
         Thread t2 = new Thread(() -> {
+            System.out.println(Thread.currentThread().isAlive());
             for (int i = 1; i <= 5; i++)
             {
                 System.out.println("Hello");
@@ -26,8 +27,18 @@ public class RunnableDemo {
                     e.printStackTrace();
                 }
             }
-        });
+        },"Second_Thread");
         t1.start();
         t2.start();
+
+        t1.join();
+        t2.join();
+        System.out.println("Exit");
+
+        System.out.println("The first thread's name is : " + t1.getName());
+        System.out.println("The second thread's name is : " + t2.getName());
+
+        System.out.println(t1.isAlive());
+        System.out.println(t2.isAlive());
     }
 }
